e8a2b37e4a2cb9cfb22a1981735da1f9abf989d1
EricFerguson76 <turtle0888@gmail.com>
2020-08-23 21:48:27 -0500

    Added to Game.js

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Game from './Components/Game';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
 			<p>Hello World</p>
+			<Game />
 		</div>
 	);
 }
--- game-of-life/src/Components/Game.js
+++ game-of-life/src/Components/Game.js
@@ -1,2 +1,67 @@
 import React, { useState, useCallback, useRef } from 'react';
 import produce from 'immer';
+
+const numRows = 30;
+const numCols = 30;
+
+const operations = [
+	[0, 1],
+	[0, -1],
+	[1, -1],
+	[-1, 1],
+	[1, 1],
+	[-1, -1],
+	[1, 0],
+	[-1, 0],
+];
+
+const EmptyGrid = () => {
+	const rows = [];
+	for (let i = 0; i < numRows; i++) {
+		rows.push(Array.from(Array(numCols), () => 0));
+	}
+	return rows;
+};
+
+function Game() {
+	const [running, setRunning] = useState(false);
+	const [speed, setSpeed] = useState(1000);
+	const [grid, setGrid] = useState(() => {
+		return EmptyGrid();
+	});
+
+	const runningRef = useRef(running);
+	runningRef.current = running;
+
+	const runSim = useCallback(() => {
+		if (!runningRef.current) {
+			return;
+		}
+
+		setGrid((g) => {
+			return produce(g, (gridCopy) => {
+				for (let i = 0; i < numRows; i++) {
+					for (let j = 0; j < numCols; j++) {
+						let neighbors = 0;
+						operations.forEach(([x, y]) => {
+							const newI = i + x;
+							const newJ = j + x;
+							if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+								neighbors += g[newI][newJ];
+							}
+						});
+						if (neighbors < 2 || neighbors > 3) {
+							gridCopy[i][j] = 0;
+						} else if (g[i][j] === 0 && neighbors === 3) {
+							gridCopy[i][j] = 1;
+						}
+					}
+				}
+			});
+		});
+		setTimeout(runSim, speed);
+	});
+	return <div></div>;
+}
+
+export default Game;
